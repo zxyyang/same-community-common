d12d6a3ea01349add01a793af765b0db3e5c129e
zixuan.yang <zixuan.yang@shihengtech.com>
2024-07-05 00:27:13 +0800

    修改更新权重的优先级在所有bean加载完后执行

--- same-community-common-util/src/main/java/com/same/community/common/util/config/NacosServiceConfig.java
+++ same-community-common-util/src/main/java/com/same/community/common/util/config/NacosServiceConfig.java
@@ -1,24 +1,26 @@
 package com.same.community.common.util.config;
 
+import cn.hutool.core.exceptions.ExceptionUtil;
 import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
 import com.alibaba.cloud.nacos.NacosServiceManager;
 import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.api.naming.pojo.Instance;
 import com.same.community.common.util.utils.IpUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 /**
- * 由Zixuan.Yang编写
- * 日期：2024/3/16
+ * @author Zixuan.Yang
+ * @date 2024/3/16 12:05
  */
 @Component
 @Slf4j
-@Order(1) // 确保这个Runner在其他Runner之前执行
-public class NacosServiceConfig implements CommandLineRunner {
+@Order(1)
+public class NacosServiceConfig implements SmartInitializingSingleton {
     @Autowired
     private NacosDiscoveryProperties discoveryProperties;
 
@@ -26,13 +28,20 @@ public class NacosServiceConfig implements CommandLineRunner {
     private NacosServiceManager nacosServiceManager;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void afterSingletonsInstantiated() {
         String serviceName = discoveryProperties.getService();
         String group = discoveryProperties.getGroup();
+        Instance instance = null;
+        try {
+            instance = nacosServiceManager.getNamingService().selectOneHealthyInstance(serviceName, group);
+        } catch (NacosException e) {
+            log.error("NACOS获取实例失败:{}", ExceptionUtil.stacktraceToString(e));
+            throw new RuntimeException(e);
+        }
 
-        Instance instance = new Instance();
-        instance.setServiceName(serviceName);
-        instance.setClusterName(group);
+        // 设置当前实例的权重
+//        double newWeight = 10.0; // 你希望设置的新权重
+//        instance.setWeight(newWeight);
 
         // 判断当前操作系统是否是linux (线上环境)
         if (System.getProperty("os.name").toLowerCase().contains("linux")) {
@@ -43,20 +52,12 @@ public class NacosServiceConfig implements CommandLineRunner {
         }
 
         try {
-            // 注册实例
+            // 更新实例权重信息
             nacosServiceManager.getNamingService().registerInstance(serviceName, group, instance);
-            log.info("NACOS实例注册成功");
-
-            // 获取一个健康的实例
-            Instance healthyInstance = nacosServiceManager.getNamingService().selectOneHealthyInstance(serviceName, group);
-
-            // 设置当前实例的权重（如果需要）
-            // double newWeight = 10.0; // 你希望设置的新权重
-            // healthyInstance.setWeight(newWeight);
-
-            log.info("NACOS获取健康实例成功: {}", healthyInstance);
+            log.info("NACOS更新实例权重信息成功");
         } catch (NacosException e) {
-            log.error("NACOS操作失败", e);
+            // 异常
+            log.error("NACOS更新实例失败", e);
         }
     }
 }
